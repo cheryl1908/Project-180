2a1e1ff8313cab90345cce264ac48155225e91a4
Cheryl Fernandes <73298909+cheryl1908@users.noreply.github.com>
2022-12-14 16:40:25 +0530

    Update ar_weather.js

--- ar_weather.js
+++ ar_weather.js
@@ -18,3 +18,22 @@ function get_coordinates() {
         window.history.back();
     }
 }
+function get_weather() {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.destination_lat}&lon=${coordinates.destination_lon}&appid=94212e971d0ca977303f8ae892224bbd`,
+        type: "get",
+        success: function (response) {
+            let name = response.name
+            let weather = response.weather[0].main
+            $("#scene_container").append(
+                `
+                    <a-entity gps-entity-place="latitude: ${steps[i].maneuver.location[1]}; longitude: ${steps[i].maneuver.location[0]};">
+                        <a-entity>
+                            <a-text height="50" value="Weather forcast is ${weather} at ${name}"></a-text>
+                        </a-entity>
+                    </a-entity>
+                `
+            )
+        }
+    })
+}
